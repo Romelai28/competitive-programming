c880b7964b156b1dc8e2d4a38799fe81563337ed
Mauricio Romero Laino <Romelai28@gmail.com>
2024-07-12 20:04:02 -0300

    template

---
+++ notebook/template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+const ll UNDEFINED = -1;
+
+const int MAX_N = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LINF = 1e18;
+const ll zero = 0;
+#define pb push_back
+#define fst first
+#define snd second
+
+int main{
+    
+    return 0;
+}
