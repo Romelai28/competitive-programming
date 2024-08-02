96c09b02ae54951c3352efaa07ddb4dbdd26aab2
Mauricio <100548962+Romelai28@users.noreply.github.com>
2024-08-02 20:24:59 -0300

    Correción en comentario de la bs

--- notebook/other/binary_search.cpp
+++ notebook/other/binary_search.cpp
@@ -3,7 +3,7 @@
 int l = -1; // extremo izquierdo del rango de búsqueda -1
 int r = n; // extremo derecho del rango de búsqueda +1
 while(r - l > 1) { // mientras que la distancia entre las fronteras sea >1 (es decir, mientras que no estén contiguas)
-    int mid = (l + r) >> 1;
+    int mid = (l + r) / 2;
     if(P(mid)) {
         l = mid;
     } else {
@@ -12,4 +12,4 @@ while(r - l > 1) { // mientras que la distancia entre las fronteras sea >1 (es d
 }
 
 // Respuesta:
-// 'a' la última para cual P da true, y 'b' la primera para la cual P da false.
+// 'l' la última para cual P da true, y 'r' la primera para la cual P da false.
