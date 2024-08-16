529473944f5a8e8ca43fe939fd60da2bedd442c6
FacundoCG <coronelfacundo30@gmail.com>
2024-08-16 16:35:15 -0300

    Añadiendo binary search de extremos

--- notebook/graph/bfs.cpp
+++ notebook/graph/bfs.cpp
@@ -1,27 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-typedef long long ll;
-const ll UNDEFINED = -1;
-
-const int MAX_N = 1e5 + 1;
-const int MOD = 1e9 + 7;
-const int INF = 1e9;
-const ll LINF = 1e18;
-const ll zero = 0;
-#define pb push_back
-#define fst first
-#define snd second
-#define esta(x,c) ((c).find(x) != (c).end())  // Devuelve true si x es un elemento de c.
-
-#define DBG(x) cerr << #x << " = " << (x) << endl
-#define RAYA cerr << "----------" << endl
-#define forn(i,n) for (int i=0;i<(int)(n);i++)
-#define forsn(i,s,n) for (int i=(s);i<(int)(n);i++)
-#define dforn(i,n) for(int i=(int)((n)-1);i>=0;i--)
-#define dforsn(i,s,n) for(int i=(int)((n)-1);i>=(int)(s);i--)
-#define all(c) (c).begin(),(c).end()
-
 // BFS Normal
 void bfs(ll inicio, vector<vector<ll>> &ady, vector<bool> &vis, vector<ll> &parent){
     queue<ll> q;
--- notebook/other/binary_search.cpp
+++ notebook/other/binary_search.cpp
@@ -1,5 +1,7 @@
 // Asumiento que quiero hacer una búsqueda binaria en el rango [0, n)
+// Importante: chequear el indice porque en caso de que no exista elemento que cumple P(X) puede haber problemas
 
+// Calcular extremo derecho que cumple P(X)
 int l = -1; // extremo izquierdo del rango de búsqueda -1
 int r = n; // extremo derecho del rango de búsqueda +1
 while(r - l > 1) { // mientras que la distancia entre las fronteras sea >1 (es decir, mientras que no estén contiguas)
@@ -10,6 +12,18 @@ while(r - l > 1) { // mientras que la distancia entre las fronteras sea >1 (es d
         r = mid;
     }
 }
+// l es el último elemento que cumple P(X)
 
-// Respuesta:
-// 'l' la última para cual P da true, y 'r' la primera para la cual P da false.
+// Calcular extremo izquierdo que cumple P(X)
+int l = -1; 
+int r = n; 
+while(r - l > 1) { 
+    int mid = (l + r) / 2;
+    if(!P(mid)) {
+        l = mid;
+    } else {
+        r = mid;
+    }
+}
+
+// r es el primer elemento que cumple P(X)
