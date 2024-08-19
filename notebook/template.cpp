f763a4f1294cd82367a88bbdb7bf5a7ac24f2110
Romelai28 <Romelai28@gmail.com>
2024-08-19 04:54:36 -0300

    reordenamiento

--- notebook/template.cpp
+++ notebook/template.cpp
@@ -3,25 +3,27 @@ using namespace std;
 
 typedef long long ll;
 typedef long double ld;
-const ll UNDEFINED = -1;
 
+const ll UNDEFINED = -1;
 const int MAX_N = 1e5 + 1;
 const int MOD = 1e9 + 7;
 const int INF = 1e9;
 const ll LINF = 1e18;
 const ll zero = 0;
+
 #define pb push_back
 #define fst first
 #define snd second
 #define esta(x,c) ((c).find(x) != (c).end())  // Devuelve true si x es un elemento de c.
+#define all(c) (c).begin(),(c).end()
 
 #define DBG(x) cerr << #x << " = " << (x) << endl
 #define RAYA cerr << "----------" << endl
+
 #define forn(i,n) for (int i=0;i<(int)(n);i++)
 #define forsn(i,s,n) for (int i=(s);i<(int)(n);i++)
 #define dforn(i,n) for(int i=(int)((n)-1);i>=0;i--)
 #define dforsn(i,s,n) for(int i=(int)((n)-1);i>=(int)(s);i--)
-#define all(c) (c).begin(),(c).end()
 
 // Show vector
 template <typename T>
