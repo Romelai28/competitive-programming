c830b41c095cc346e6c90370b82c224c303c45d6
Romelai28 <Romelai28@gmail.com>
2026-08-27 05:54:14 -0300

    Agrega gardgets para 2SAT

--- notebook/graph/2sat.cpp
+++ notebook/graph/2sat.cpp
@@ -1,14 +1,9 @@
-// Requiere SCC.
-// O(n)
-
+// Requiere SCC. // O(n)
 struct TwoSat {
 	int n_vars, n_vertices;
-	vvi ady, ady_t;
-	vb assignment;
-	
+	vvi ady, ady_t; vb assignment;
 	TwoSat(int n) : n_vars(n), n_vertices(2*n), ady(n_vertices), ady_t(n_vertices), assignment(n_vars) {}
 	// indexo las variables como: V0, F0, V1, F1, V2, F2 ...
-	
 	bool canBeSat(){
 		SCC scc(ady, ady_t);
 		forn(i, n_vars){
@@ -17,9 +12,8 @@ struct TwoSat {
 		}
 		return true;
 	}
-	
 	void add_disyuncion(int a, bool na, int b, bool nb){
-		// na true si tengo que poner (not A) na false si tengo que poner A (!!!!!)
+		// na true si tengo que poner (not A) na false si tengo que poner A!
 		a = (2*a) ^ na; b = (2*b) ^ nb;
 		int neg_a = a ^ 1, neg_b = b ^ 1;
 		// A or B <=> ((not B => A) and (not A => B))
@@ -27,3 +21,9 @@ struct TwoSat {
 		ady[neg_a].pb(b); ady_t[b].pb(neg_a);
 	}
 };
+// Gadgets:
+// 1. Forcing a variable to be true: (x or x)
+// 2. Exactly one variable to be true: (x or y) and (not x or not y)
+// 3. At least one variable to be true: (x or y)
+// 4. At most one variable to be true: (not x or not y)
+// 5. Both variables must have the same value: (x or not y) and (not x or y)
