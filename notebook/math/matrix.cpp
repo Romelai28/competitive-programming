523333af8d18d86901bcfcb8ee69c05ed8dbbda7
Romelai28 <Romelai28@gmail.com>
2026-08-27 05:07:43 -0300

    Acorta implementación de matrices

--- notebook/math/matrix.cpp
+++ notebook/math/matrix.cpp
@@ -1,9 +1,6 @@
 using matrix_t = int;
-
 struct Mat {
-	int n, m;
-	vector<vector<matrix_t>> a;
-	
+	int n, m; vv<matrix_t> a;
 	Mat() { }
 	// Inicializa matriz de nxm con todos 0.
 	Mat(int _n, int _m){
@@ -11,100 +8,49 @@ struct Mat {
 		a.assign(n, vector<matrix_t>(m, 0));
 	}
 	// Inicializa una matriz a partir de un vector.
-	Mat(vector< vector<matrix_t> > v){
-		n = v.size();
-		m = n ? v[0].size() : 0;
+	Mat(const vv<matrix_t> &v){
+		n = SIZE(v);
+		m = n ? SIZE(v[0]) : 0;
 		a = v;
 	}
-	
 	// Constructor de la identidad.  (Puedo hacer Mat I = Mat::identity(4);)
 	static Mat identity(int _n) {
 		Mat res(_n, _n);
-		for (int i = 0; i < _n; ++i){
-			res.a[i][i] = 1;
-		}
+		forn(i, _n) {res.a[i][i] = 1;}
 		return res;
 	}
-		
-	// Constructores de matrices vectores columnas.
-	static Mat crearVectorColumna(vector<matrix_t> v) {  // ej: Mat inicial = Mat::crearVectorColumna(vector<int> {1, 1});
-		Mat res(SIZE(v), 1);
-		for (int i=0; i<SIZE(v); i++){
-			res.a[i][0] = v[i];
-		}
-		return res;
-	}
-	
-	inline Mat operator + (const Mat &b) {
+	Mat operator + (const Mat &b) {
 		assert(n == b.n && m == b.m);
 		Mat ans = Mat(n, m);
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < m; j++) {
-				ans.a[i][j] = (a[i][j] + b.a[i][j]) % MOD;
-			}
-		}
+		forn(i, n) forn(j, m) {ans.a[i][j] = (a[i][j] + b.a[i][j]) % MOD;}
 		return ans;
     }
-    
-    inline Mat operator - (const Mat &b) {
+    Mat operator - (const Mat &b) {
 		assert(n == b.n && m == b.m);
 		Mat ans = Mat(n, m);
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < m; j++) {
-				ans.a[i][j] = (a[i][j] - b.a[i][j]) % MOD;
-			}
-		}
+		forn(i, n) forn(j, m) {ans.a[i][j] = (a[i][j] - b.a[i][j] + MOD) % MOD;}
 		return ans;
     }
-    
-    inline Mat operator * (const Mat &b) {
+    Mat operator * (const Mat &b) {
 		assert(m == b.n);
 		Mat ans = Mat(n, b.m);
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < b.m; j++) {
-				for(int k = 0; k < m; k++) {
-					ans.a[i][j] = (ans.a[i][j] + 1LL * a[i][k] * b.a[k][j] % MOD) % MOD;
-				}
-			}
-		}
+		forn(i, n) forn(j, b.m) forn(k, m) {ans.a[i][j] = (ans.a[i][j] + 1LL * a[i][k] * b.a[k][j] % MOD) % MOD;}
 		return ans;
 	}
-	
-	inline Mat pow(long long expo) {
+	Mat pow(ll expo) {
 		assert(n == m);
 		Mat ans = identity(n);
 		Mat t(a);
-		
 		while (expo) {
-			if (expo & 1){
-				ans = ans * t;
-			}
+			if (expo & 1) {ans = ans * t;}
 			t = t * t;
 			expo >>= 1;
 		}
 		return ans;
 	}
-	
-	inline Mat& operator += (const Mat& b) { return *this = (*this) + b; }
-	inline Mat& operator -= (const Mat& b) { return *this = (*this) - b; }
-	inline Mat& operator *= (const Mat& b) { return *this = (*this) * b; }
-	inline bool operator == (const Mat& b) { return a == b.a; }
-	inline bool operator != (const Mat& b) { return a != b.a; }
-	
-	// Sobrecarga del operador <<
-	friend ostream& operator<<(ostream& os, const Mat& mat) {
-		for (int i = 0; i < mat.n; i++) {
-			cout << "[";
-			for (int j = 0; j < mat.m; j++) {
-				os << mat.a[i][j] << ' ';
-			}
-			cout << "]";
-			os << '\n';
-		}
-		return os;
-	}
-	
-	// Acceso como M[i][j]
-	inline vector<matrix_t>& operator[](int i) { return a[i]; }
-	inline const vector<matrix_t>& operator[](int i) const { return a[i]; }
 };
+// Min-plus (tropical) matrix multiplication:
+//   (A * B)[i][j] = min_k(A[i][k] + B[k][j])
+// Identity: 0 on diagonal, INF elsewhere.
+// A^k[i][j] = minimum cost of a path of exactly k edges.
+// Producto pasa a ser la Suma. Suma pasa a ser el Min.
